3930f740c512d3743a0d5a800be20ddc9c6a9095
Kyrus1999 <71381995+Kyrus1999@users.noreply.github.com>
2022-05-24 14:50:54 +0200

    Changed AdaptiveQueue to BucktQueue to get deterministic results.

--- src/search/heuristics/additive_heuristic.h
+++ src/search/heuristics/additive_heuristic.h
@@ -22,7 +22,7 @@ class AdditiveHeuristic : public relaxation_heuristic::RelaxationHeuristic {
        below the signed 32-bit int upper bound.
      */
     static const int MAX_COST_VALUE = 100000000;
-    priority_queues::AdaptiveQueue<PropositionNode*> queue;
+    priority_queues::BucketQueue<PropositionNode*> queue;
     bool did_write_overflow_warning;
 
     void setup_exploration_queue(const State &state);
