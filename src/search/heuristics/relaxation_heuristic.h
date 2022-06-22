3b8541e7d2293b27be770d028fcb77c44e53fc08
Kyrus1999 <71381995+Kyrus1999@users.noreply.github.com>
2022-06-22 11:51:07 +0200

    -Changed BucketQueue back to AdaptiveQueue

--- src/search/heuristics/relaxation_heuristic.h
+++ src/search/heuristics/relaxation_heuristic.h
@@ -26,7 +26,7 @@ class RelaxationHeuristic;
 struct PropositionNode;
 
 
-using PropQueue = priority_queues::BucketQueue<PropositionNode*>;
+using PropQueue = priority_queues::AdaptiveQueue<PropositionNode*>;
 
 struct GraphNode {
     int cost; // Used for h^max cost or h^add cost;
